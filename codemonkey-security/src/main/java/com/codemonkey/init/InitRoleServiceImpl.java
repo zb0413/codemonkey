53633078e956a2ac683e57fef532fb575fc80742
zhaobin <13342280327@163.com>
2018-08-09 17:10:30 +0800

    init role with function nodes

--- codemonkey-security/src/main/java/com/codemonkey/init/InitRoleServiceImpl.java
+++ codemonkey-security/src/main/java/com/codemonkey/init/InitRoleServiceImpl.java
@@ -1,5 +1,8 @@
 package com.codemonkey.init;
 
+import java.util.List;
+import java.util.Set;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,8 +10,9 @@ import org.springframework.stereotype.Component;
 
 import com.codemonkey.config.SecurityConfig;
 import com.codemonkey.domain.AppRole;
-import com.codemonkey.repository.AppRoleRepository;
-import com.codemonkey.repository.FunctionNodeRepository;
+import com.codemonkey.domain.FunctionNode;
+import com.codemonkey.service.AppRoleService;
+import com.codemonkey.service.FunctionNodeService;
 import com.codemonkey.utils.SysUtils;
 
 @Component
@@ -16,21 +20,41 @@ import com.codemonkey.utils.SysUtils;
 @Transactional
 public class InitRoleServiceImpl implements InitDataBean{
 
-	@Autowired private AppRoleRepository appRoleRepository;
-	@Autowired private FunctionNodeRepository functionNodeRepository;
+	@Autowired private AppRoleService appRoleService;
+	@Autowired private FunctionNodeService functionNodeService;
 	@Autowired private SecurityConfig securityConfig;
 	
 	@Override
 	public void doInit() {
 		if(!SysUtils.fileExists(SysUtils.DATA_INITED)){
-			AppRole adminRole = appRoleRepository.findByCode(securityConfig.getAdminRole());
+			AppRole adminRole = appRoleService.findBy("code" , securityConfig.getAdminRole());
 			if (adminRole == null) {
 				adminRole = new AppRole();
 			}else{
 				adminRole.setCode(securityConfig.getAdminRole());
 				adminRole.setName("系统管理员");
 			}
-			appRoleRepository.save(adminRole);
+			List<FunctionNode> functionNodes = functionNodeService.findAll();
+			addAllFunctionNodes(adminRole , functionNodes);
+			appRoleService.save(adminRole);
+		}
+	}
+
+	private void addAllFunctionNodes(AppRole adminRole, List<FunctionNode> functionNodes) {
+		Set<FunctionNode> set = adminRole.getFunctionNodes();
+		
+		if(SysUtils.isEmpty(set)) {
+			if(SysUtils.isNotEmpty(functionNodes)) {
+				set.addAll(functionNodes);
+			}
+		}
+		
+		if(SysUtils.isNotEmpty(functionNodes)) {
+			for(FunctionNode fn : functionNodes) {
+				if(SysUtils.isNotIncluded(functionNodes, fn)) {
+					set.add(fn);
+				}
+			}
 		}
 	}
 }
